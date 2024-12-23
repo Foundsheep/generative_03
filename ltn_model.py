6b58c83d0b1dcea4526519fe357c56a3bf4d48d6
Foundsheep <moondeokjong@gmail.com>
2024-12-23 11:53:07 +0900

    fix: real and fake iamges' dtype

--- ltn_model.py
+++ ltn_model.py
@@ -89,8 +89,7 @@ class CustomDDPM(L.LightningModule):
         # fake_image = normalise_to_zero_and_one_from_255(fake_image)
 
         fid = get_fid(fake_image, real_image, self.device)
-        print(f"{type(fake_image) = } / {type(real_image) = }")
-        loss = self.loss_fn(fake_image, real_image)
+        loss = self.loss_fn(fake_image.to(dtype=torch.float32), real_image.to(dtype=torch.float32))
         self.log("val_loss", loss, prog_bar=True, on_epoch=True)
         self.log("val_fid", fid, prog_bar=True, on_epoch=True)
         return 
