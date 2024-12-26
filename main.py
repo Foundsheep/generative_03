c69d4ef73e86b8e5ad43f46f830f998e2b803a25
Foundsheep <moondeokjong@gmail.com>
2024-12-26 10:28:18 +0900

    testing: CUDA driver error

--- main.py
+++ main.py
@@ -63,6 +63,9 @@ def predict(args):
     
     # to resolve OOM error
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+    os.environ["CUDA_HOME"] = "/usr/local/cuda"
+    os.environ["PATH"] = os.environ["CUDA_HOME"] + "/bin:" + os.environ["PATH"]
+    os.environ["LD_LIBRARY_PATH"] = os.environ["CUDA_HOME"] + "/lib64:" + os.environ["LD_LIBRARY_PATH"]
     print(f"**** {os.environ['PYTORCH_CUDA_ALLOC_CONF'] = }")
         
     model = CustomDDPM.load_from_checkpoint(
